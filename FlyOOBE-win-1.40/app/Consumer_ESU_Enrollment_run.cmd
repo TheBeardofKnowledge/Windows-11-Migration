d6095d09accf0e883f3186c62f112175d5843242
The Beard of Knowledge <thebeardofknowledge956@gmail.com>
2025-12-02 14:22:18 -0600

    Delete FlyOOBE-win-1.40 directory

--- FlyOOBE-win-1.40/app/Consumer_ESU_Enrollment_run.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set "_PSf=%~dp0Consumer_ESU_Enrollment.ps1"
-setlocal EnableDelayedExpansion
-set "_PSf=!_PSf:'=''!"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ^& "'!_PSf!' %*"
